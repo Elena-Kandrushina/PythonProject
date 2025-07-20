16116427146bb09fe8f34417170203ea06ca226f
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-07-20 13:02:43 +0300

    formatted code

--- src/widget.py
+++ src/widget.py
@@ -11,10 +11,9 @@ def mask_account_card(account_card: str) -> str:
         tip_card = f"{account_card[:-16]}"
 
         card_number = f"{account_card[-17:-1]}"
-        card_number  = get_mask_card_number(card_number)
+        card_number = get_mask_card_number(card_number)
         mask_account_card = tip_card + card_number
         return mask_account_card
-
 
 
 if __name__ == "__main__":
@@ -23,8 +22,8 @@ if __name__ == "__main__":
 
 def get_date(date_t02: str) -> str:
     """Функция принимает на вход строку с датой в формате
-2024-03-11T02:26:18.671407 и возвращает строку с датой в формате
-ДД.ММ.ГГГГ"""
+    2024-03-11T02:26:18.671407 и возвращает строку с датой в формате
+    ДД.ММ.ГГГГ"""
     date_1 = f"{date_t02[:10]}"
     date_2 = f"{date_1[8:10]}{date_1[4:8]}{date_1[:4]}"
 
@@ -33,4 +32,3 @@ def get_date(date_t02: str) -> str:
 
 if __name__ == "__main__":
     print(get_date("2025-07-20T02:26:18.671407"))
-
